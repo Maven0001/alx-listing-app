e5f0b442645325016a5422319f33ebda584d72de
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-22 15:17:39 +0100

    listing app

--- alx-listing-app/components/common/SearchField/SearchField.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import "./SearchField.css";
-
-const SearchField = () => {
-  return <div className="Search-field-container">HELLO</div>;
-};
-
-export default SearchField;
